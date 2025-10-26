d4faeb138689df704815944bd56c8c104756f9c2
ruenh <ruenhoff@gmail.com>
2025-10-26 11:01:20 +0300

    healthcheck api/ping + MiniApp test

---
+++ api/ping.py
@@ -0,0 +1,9 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({'ok': True}).encode())
